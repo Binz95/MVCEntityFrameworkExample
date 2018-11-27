692fa82fdf0a7b01fa0d987c876973fc961fb2ab
Binz95 <binianiejacob1995@gmail.com>
2018-11-27 16:12:38 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkEg.Models;
+namespace EntityFrameworkEg.Controllers
+{
+    public class HomeController : Controller
+    {
+        ContactManagementSimplifiedEntities ctx = new ContactManagementSimplifiedEntities();
+        public ActionResult Index()
+        {
+            return View(ctx.Contacts.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+           return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Contact c)
+        {
+            ctx.Contacts.Add(c);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFrameworkEg.MvcApplication" Language="C#" %>
